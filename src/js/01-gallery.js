06eb9881748a2c32c9d0ea905a1738f4792cfe05
Ksiushka <pupusia88@gmail.com>
2023-04-20 08:52:27 +0200

    mistake2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,4 +3,4 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
-import "simplelightbox/dist/simple-lightbox.min.css"
+// import "simplelightbox/dist/simple-lightbox.min.css"
